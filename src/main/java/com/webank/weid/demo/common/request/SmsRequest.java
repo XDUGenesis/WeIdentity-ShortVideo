d955bdc272a81db8d0d996f9851daef08acaf33d
hkxia95 <hongkangxia@163.com>
2020-10-07 20:38:48 +0800

    # SmsController # SmsRequest

---
+++ src/main/java/com/webank/weid/demo/common/request/SmsRequest.java
@@ -0,0 +1,21 @@
+package com.webank.weid.demo.common.request;
+
+import java.sql.Timestamp;
+
+/**
+ * @Author: hkxia
+ * @Date: 2020/10/7
+ * @Version 1.0
+ */
+public class SmsRequest {
+
+    // 注册页面手机号
+    private String phoneNum;
+
+    // 注册页面输入密码
+    private String userPwd;
+
+    // 注册页面验证码
+    private String password;
+
+}
